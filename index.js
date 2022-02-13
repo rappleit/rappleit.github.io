9658645db14f041a5f14dbb0b9a3b197f3acbe50
Rappleit <rachel.limre03@gmail.com>
2022-02-14 02:45:00 +0800

    added index.js, animation to social buttons

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#socialButtonUL li:nth-child(1)").delay(1000).animate({opacity: 1}, 200);
+    $("#socialButtonUL li:nth-child(2)").delay(1300).animate({opacity: 1}, 200);
+    $("#socialButtonUL li:nth-child(3)").delay(1600).animate({opacity: 1}, 200);
+  });
+
