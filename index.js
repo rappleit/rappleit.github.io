5bc91323ee79fcc0d4bbee8d145b9d4b97921364
Rappleit <rachel.limre03@gmail.com>
2022-02-14 14:32:39 +0800

    added animated navbar

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
+
     $("#socialButtonUL li:nth-child(1)").delay(1000).animate({opacity: 1}, 200);
     $("#socialButtonUL li:nth-child(2)").delay(1300).animate({opacity: 1}, 200);
     $("#socialButtonUL li:nth-child(3)").delay(1600).animate({opacity: 1}, 200);
+
+    $("#aboutMe").delay(900).animate({opacity: 1}, 400);
+    $("#aboutMe").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#aboutMeLabel").delay(1600).animate({opacity: 1}, 200);
+
+    $("#experience").delay(1100).animate({opacity: 1}, 400);
+    $("#experience").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#experienceLabel").delay(1900).animate({opacity: 1}, 200);
+
+    $("#projects").delay(1300).animate({opacity: 1}, 400);
+    $("#projects").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#projectsLabel").delay(2200).animate({opacity: 1}, 200);
   });
 
