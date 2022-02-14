63d59d37119000f6fa2182ea86877807fcf4857e
Rappleit <rachel.limre03@gmail.com>
2022-02-15 00:10:56 +0800

    added intro section under about.html

--- about.js
+++ about.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
+    $("header").delay(500).animate({opacity: 1}, 400);
     $(".scrollDown").delay(1000).animate({opacity: 1}, 400);
 });
--- navBar.js
+++ navBar.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     $("#aboutMe").delay(900).animate({opacity: 1}, 400);
-    $("#aboutMe").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#aboutMe").animate({width: "30vw", borderRadius: "40px"}, 400);
     $("#aboutMeLabel").delay(1600).animate({opacity: 1}, 200);
 
     $("#experience").delay(1100).animate({opacity: 1}, 400);
-    $("#experience").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#experience").animate({width: "30vw", borderRadius: "40px"}, 400);
     $("#experienceLabel").delay(1900).animate({opacity: 1}, 200);
 
     $("#projects").delay(1300).animate({opacity: 1}, 400);
-    $("#projects").animate({width: "40vh", borderRadius: "40px"}, 400);
+    $("#projects").animate({width: "30vw", borderRadius: "40px"}, 400);
     $("#projectsLabel").delay(2200).animate({opacity: 1}, 200);
   });
 
