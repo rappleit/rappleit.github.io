1b95c584ad736233cbaa4816d41033cf022745f6
Rappleit <rachel.limre03@gmail.com>
2022-02-14 19:21:20 +0800

    shifted navBar styles and code to navBar.css and navBar.js

--- navBar.js
+++ navBar.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-    $("#socialButtonUL li:nth-child(1)").delay(1000).animate({opacity: 1}, 200);
-    $("#socialButtonUL li:nth-child(2)").delay(1300).animate({opacity: 1}, 200);
-    $("#socialButtonUL li:nth-child(3)").delay(1600).animate({opacity: 1}, 200);
-
     $("#aboutMe").delay(900).animate({opacity: 1}, 400);
     $("#aboutMe").animate({width: "40vh", borderRadius: "40px"}, 400);
     $("#aboutMeLabel").delay(1600).animate({opacity: 1}, 200);
