039947c2a640ba6c9273763087575fd7f80bc31a
Rappleit <rachel.limre03@gmail.com>
2022-02-22 11:41:56 +0800

    added scrolldown indicator

--- about.js
+++ about.js
@@ -2,3 +2,12 @@ $(document).ready(function() {
     $("header").delay(500).animate({opacity: 1}, 400);
     $(".scrollDown").delay(1000).animate({opacity: 1}, 400);
 });
+
+$(window).scroll(function(){
+    if($(this).scrollTop() < this.innerHeight - 70 ){
+        $('.scrollDownLabel').css({'opacity': '0'});
+      }
+      else if ($(this).scrollTop() >= this.innerHeight - 70){
+        $('.scrollDownLabel').css({'opacity': '0.75'});
+      }
+})
