9c0dbb5b6e3e69a3070f62084b4113f38a98c0ee
Rappleit <rachel.limre03@gmail.com>
2022-03-25 02:56:05 +0800

    created navigation dropdown menu

--- nav.js
+++ nav.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
-    $(".navOpenButton a").on('click', function(){
-      const visibility = $(".navBar").attr("data-visible");
-        
-      if (visibility === "false") {
-        $(".navBar").attr("data-visible", "true");
-        $('.navOpenButton').css({'display': 'none'});
-      } else if (visibility === "true") {
-        $(".navBar").attr("data-visible", "false");
-        $('.navOpenButton').css({'display': 'block'});
-      }
-    });
+    $(".navToggle").on('click', function(){
+        const activated = $(".navToggle").attr("data-activated");
+        console.log($(".navToggle").attr("data-activated"))
 
+        if (activated === "false") {
+            $(".navBarOptions").css({'opacity': '1', 'transform': 'translateY(0px)'});
+            $("#navToggleIcon").removeClass("fa-bars");
+            $("#navToggleIcon").addClass("fa-xmark");
+            $(".navToggle").attr("data-activated", "true");
+        } else if (activated === "true") {
+            $(".navBarOptions").css({'opacity': '0', 'transform': 'translateY(-20px)'});
+            $("#navToggleIcon").removeClass("fa-xmark");
+            $("#navToggleIcon").addClass("fa-bars");
+            $(".navToggle").attr("data-activated", "false");
+        }
+        
+    });
   });
